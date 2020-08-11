183605bc717baf34fe44d8ac9e99a4ffd656867b
Mateus Romano <mateusyasd@hotmail.com>
2020-08-11 19:41:04 -0300

    Updates

---
+++ precache-manifest.4708241dfc603eea9549c7832dc86f4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3436dc64a13cd42e3c749ef4477fa64e",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "aa03e171eb4d172c9c13",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "93d0465cc0473b7cc836",
+    "url": "/react-gh-pages/static/js/2.9c2ace91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.9c2ace91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa03e171eb4d172c9c13",
+    "url": "/react-gh-pages/static/js/main.cea05eae.chunk.js"
+  },
+  {
+    "revision": "e376be69273f6c169b62",
+    "url": "/react-gh-pages/static/js/runtime-main.584b355c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
